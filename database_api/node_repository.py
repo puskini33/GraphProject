876291d577e56c1160c3697c2a26c13ee3ac9dfd
puskini33 <elenahirjoaba@yahoo.com>
2020-02-12 15:00:24 +0100

    changed: class inherits from BaseRepository

--- database_api/node_repository.py
+++ database_api/node_repository.py
@@ -1,36 +1,34 @@
-class NodeRepository(object):
+from database_api.base_repository import BaseRepository
 
-    def insert(self, name, graph_id):
-        inserted_values = f"INSERT INTO node (name, graph_id) VALUES ('{name}', '{graph_id}');"
-        return inserted_values
 
-    def get_node(self, node_id):
+class NodeRepository(BaseRepository):
+
+    def __init__(self, path):
+        super().__init__(path)
+
+    def insert_node(self, node_name: str, graph_id: int):
+        inserted_values = f"INSERT OR IGNORE INTO node (name, graph_id) VALUES ('{node_name}', '{graph_id}');"
+        self.execute_query(inserted_values)
+
+    def get_node(self, node_id: int) -> list:
         node_values = f"SELECT * FROM node WHERE node.id = '{node_id}';"
-        return node_values
+        self.execute_query(node_values)
+        return self.cursor.fetchall()
 
-    def get_graph_nodes(self, graph_id):
+    def get_graph_nodes(self, graph_id: int) -> list:
         graph_nodes = f"SELECT graph.id AS graph_id, node.id AS node_id, node.name AS node_name "\
                       f"FROM node "\
                       f"JOIN graph "\
                       f"ON node.graph_id = graph.id "\
                       f"WHERE graph.id = '{graph_id}';"
-        return graph_nodes
+        self.execute_query(graph_nodes)
+        return self.cursor.fetchall()
 
-    def get_node_edges(self, node_id):
-        node_edges_values = f"SELECT node.name AS node_name, " \
-                            f"edge.name AS edge_name " \
-                            f"FROM node " \
-                            f"JOIN graph " \
-                            f"ON node.graph_id = graph.id " \
-                            f"JOIN edge " \
-                            f"ON edge.node_start_id = node.id " \
-                            f"WHERE node.id = '{node_id}';"
-        return node_edges_values
-
-    def update(self, node_id, node_name, graph_id):
+    def update_node(self, node_id: int, node_name: str, graph_id: int):
         updated_values = f"UPDATE node SET name = '{node_name}', graph_id = '{graph_id}'  WHERE id = '{node_id}';"
-        return updated_values
+        self.execute_query(updated_values)
 
-    def delete(self, node_id):
+    def delete_node(self, node_id: int):
         deleted_values = f"DELETE FROM node WHERE id = '{node_id}';"
-        return deleted_values
+        self.execute_query(deleted_values)
+
