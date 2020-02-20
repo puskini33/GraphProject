cbd0d7c19afcdae4639183763b682a69e30c409e
puskini33 <elenahirjoaba@yahoo.com>
2020-02-21 00:12:40 +0100

    change functions of CRUD operations

--- business_service/graph_business_service.py
+++ business_service/graph_business_service.py
@@ -8,18 +8,18 @@ class GraphBusinessService(object):
     def __init__(self, in_graph_repository: GraphRepository):
         self.graph_repository = in_graph_repository
 
-    def insert_graph_model(self, graph_model: GraphModel, graph_name: str) -> GraphModel:
-        graph_id = self.graph_repository.insert_graph(graph_name)
-        return GraphMapHelper.graph_model_db_entity(graph_model, graph_id, graph_name)
+    def insert_graph(self, graph_model: GraphModel) -> GraphModel:
+        graph_id = self.graph_repository.insert_graph(graph_model.graph_name)
+        graph_model.graph_id = graph_id
+        return graph_model
 
     def get_graph_model(self, graph_id: int) -> GraphModel:
         list_values_graph = self.graph_repository.get_graph(graph_id)
         return GraphMapHelper.db_entity_to_graph_model(list_values_graph)
 
-    def update_graph_model(self, graph_model: GraphModel, graph_id: int, graph_name: str):
-        self.graph_repository.update_graph(graph_id, graph_name)
-        return GraphMapHelper.graph_model_db_entity(graph_model, graph_id, graph_name)
+    def update_graph(self, graph_model: GraphModel) -> GraphModel:
+        self.graph_repository.update_graph(graph_model.graph_id, graph_model.graph_name)
+        return graph_model
 
-    def delete_graph_model(self, graph_model: GraphModel, graph_id: int):
+    def delete_graph(self, graph_id: int):
         self.graph_repository.delete_graph(graph_id)
-        del graph_model
--- business_service/map_helpers/graph_map_helper.py
+++ business_service/map_helpers/graph_map_helper.py
@@ -4,14 +4,7 @@ from models.graph_model import GraphModel
 class GraphMapHelper(object):
     @staticmethod
     def db_entity_to_graph_model(db_entity: list):
-        graph_model = GraphModel()  # create the GraphModel object
-        graph_model.graph_id = db_entity[0][0]  # set the id of the GraphModel Object
-        graph_model.graph_name = db_entity[0][1]  # set the name of the GraphModel Object
+        graph_model = GraphModel()
+        graph_model.graph_id = db_entity[0][0]
+        graph_model.graph_name = db_entity[0][1]
         return graph_model
-
-    @staticmethod
-    def graph_model_db_entity(graph_model, graph_id, graph_name):
-        graph_model.graph_id = graph_id
-        graph_model.graph_name = graph_name
-        return graph_model
-
