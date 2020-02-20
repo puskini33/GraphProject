7cc016c234527a73776a5903135abd2de70ff2cc
puskini33 <elenahirjoaba@yahoo.com>
2020-02-20 12:49:13 +0100

    add first version of the rest of CRUD operations

--- business_service/graph_business_service.py
+++ business_service/graph_business_service.py
@@ -1,14 +1,5 @@
 from database_api.graph_repository import GraphRepository
-from models.graph_model import GraphModel
-
-
-class GraphMapHelper(object):
-    @staticmethod
-    def db_entity_to_graph_model(db_entity: list):
-        graph_model = GraphModel()  # create the GraphModel object
-        graph_model.graph_id = db_entity[0][0]  # set the id of the GraphModel Object
-        graph_model.graph_name = db_entity[0][1]  # set the name of the GraphModel Object
-        return graph_model
+from business_service.map_helpers.graph_map_helper import GraphMapHelper
 
 
 class GraphBusinessService(object):
@@ -16,6 +7,18 @@ class GraphBusinessService(object):
     def __init__(self, in_graph_repository: GraphRepository):
         self.graph_repository = in_graph_repository  # initialize graph_repository
 
+    def insert_graph_model(self, graph_id, graph_name):
+        self.graph_repository.insert_graph(graph_id)
+        return GraphMapHelper.graph_model_to_db_entity(graph_id, graph_name)
+
     def get_graph_model(self, graph_id: str):
         list_values_graph = self.graph_repository.get_graph(graph_id)  # a list of graph_values is returned
         return GraphMapHelper.db_entity_to_graph_model(list_values_graph)  # create the new GraphModel object with the values
+
+    def update_graph_model(self, graph_id, graph_name):
+        self.graph_repository.update_graph(graph_id, graph_name)
+        list_values_graph = self.graph_repository.get_graph(graph_id)
+        return GraphMapHelper.db_entity_to_graph_model(list_values_graph)
+
+    def delete_graph_model(self, graph_id):
+        self.graph_repository.delete_graph(graph_id)
