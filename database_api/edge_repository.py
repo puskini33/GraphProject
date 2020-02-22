7c7cd4ddd9ca022c5ff7fc4ba33e29a162fdc663
puskini33 <elenahirjoaba@yahoo.com>
2020-02-22 18:26:32 +0100

    update repository

--- database_api/edge_repository.py
+++ database_api/edge_repository.py
@@ -6,37 +6,36 @@ class EdgeRepository(BaseRepository):
     def __init__(self, path: str):
         super().__init__(path)
 
-    def insert_edge(self, edge_name: str, edge_cost: int, node_start_id: str, node_end_id: str, graph_id: str) -> str:
-        inserted_values = f"INSERT INTO edge (name, cost, node_start_id, node_end_id, graph_id) " \
-                          f"VALUES ('{edge_name}', '{edge_cost}', '{node_start_id}', '{node_end_id}', '{graph_id}');"
+    def insert_edge(self, edge_name: str, edge_cost: int, start_node_id: int, end_node_id: int, graph_id: int) -> int:
+        inserted_values = f"INSERT INTO edge (name, cost, start_node_id, end_node_id, graph_id) " \
+                          f"VALUES ('{edge_name}', '{edge_cost}', '{start_node_id}', '{end_node_id}', '{graph_id}');"
         self.execute_query(inserted_values)
         self.execute_query("SELECT last_insert_rowid();")
         return self.cursor.fetchall()[0][0]
 
-    def get_edge_values(self, edge_id: str) -> list or tuple:
+    def get_edge_values(self, edge_id: int) -> list or tuple:
         edge_values = f"SELECT * FROM edge WHERE edge.id = '{edge_id}';"
         self.execute_query(edge_values)
         return self.cursor.fetchall()
 
-    def get_node_edges(self, node_id: str) -> list or tuple:
-        node_edges_values = f"SELECT node.name AS node_name, " \
-                            f"edge.name AS edge_name " \
+    def get_node_edges(self, node_id: int) -> list or tuple:
+        node_edges_values = f"SELECT edge.id, edge.name, edge.cost, edge.start_node_id, edge.end_node_id, edge.graph_id " \
                             f"FROM node " \
                             f"JOIN graph " \
                             f"ON node.graph_id = graph.id " \
                             f"JOIN edge " \
-                            f"ON edge.node_start_id = node.id " \
+                            f"ON edge.start_node_id = node.id " \
                             f"WHERE node.id = '{node_id}';"
         self.execute_query(node_edges_values)
         return self.cursor.fetchall()
 
-    def update_edge(self, edge_id: str, edge_name: str, edge_cost: int, node_start_id: str, node_end_id: str,
-                    graph_id: str):
+    def update_edge(self, edge_id: int, edge_name: str, edge_cost: int, start_node_id: int, end_node_id: int,
+                    graph_id: int):
         updated_values = f"UPDATE edge " \
-                         f"SET name = '{edge_name}', cost = '{edge_cost}', node_start_id = '{node_start_id}', " \
-                         f" node_end_id = '{node_end_id}', graph_id = '{graph_id}'  WHERE id = '{edge_id}';"
+                         f"SET name = '{edge_name}', cost = '{edge_cost}', start_node_id = '{start_node_id}', " \
+                         f" end_node_id = '{end_node_id}', graph_id = '{graph_id}'  WHERE id = '{edge_id}';"
         self.execute_query(updated_values)
 
-    def delete_edge(self, edge_id: str):
+    def delete_edge(self, edge_id: int):
         deleted_values = f"DELETE FROM edge WHERE id = '{edge_id}';"
         self.execute_query(deleted_values)
--- database_api/node_repository.py
+++ database_api/node_repository.py
@@ -6,19 +6,19 @@ class NodeRepository(BaseRepository):
     def __init__(self, path: str):
         super().__init__(path)
 
-    def insert_node(self, node_name: str, graph_id: str):
+    def insert_node(self, node_name: str, graph_id: int):
         inserted_values = f"INSERT OR IGNORE INTO node (name, graph_id) VALUES ('{node_name}', '{graph_id}');"
         self.execute_query(inserted_values)
         self.execute_query("SELECT last_insert_rowid();")
         return self.cursor.fetchall()[0][0]
 
-    def get_node(self, node_id: str) -> list:
+    def get_node(self, node_id: int) -> list:
         node_values = f"SELECT * FROM node WHERE node.id = '{node_id}';"
         self.execute_query(node_values)
         return self.cursor.fetchall()
 
-    def get_graph_nodes(self, graph_id: str) -> list:
-        graph_nodes = f"SELECT graph.id AS graph_id, node.id AS node_id, node.name AS node_name "\
+    def get_graph_nodes(self, graph_id: int) -> list:
+        graph_nodes = f"SELECT node.id AS node_id, node.name AS node_name, graph.id AS graph_id "\
                       f"FROM node "\
                       f"JOIN graph "\
                       f"ON node.graph_id = graph.id "\
@@ -26,10 +26,10 @@ class NodeRepository(BaseRepository):
         self.execute_query(graph_nodes)
         return self.cursor.fetchall()
 
-    def update_node(self, node_id: str, node_name: str, graph_id: str):
+    def update_node(self, node_id: int, node_name: str, graph_id: int):
         updated_values = f"UPDATE node SET name = '{node_name}', graph_id = '{graph_id}'  WHERE id = '{node_id}';"
         self.execute_query(updated_values)
 
-    def delete_node(self, node_id: str):
+    def delete_node(self, node_id: int):
         deleted_values = f"DELETE FROM node WHERE id = '{node_id}';"
         self.execute_query(deleted_values)
