f270cb63648f17c75ae168f46915568fe7c4b81b
puskini33 <elenahirjoaba@yahoo.com>
2020-02-26 16:35:05 +0100

    update main

--- view/main.py
+++ view/main.py
@@ -1,3 +1,12 @@
 from tkinter import *
-from view.window import Window
 
+
+from view.draw_circle import DrawCircle
+from view.draw_line import DrawLine
+
+my_window = Tk()
+my_canvas = Canvas(my_window, width=600, height=600, bg='white')
+my_canvas.grid(row=0, column=0)
+my_drawn_circle = DrawCircle(my_canvas)
+my_drawn_line = DrawLine(my_canvas)
+my_window.mainloop()
