5a6f444d00a43c8497c1d4bc598cc234f4db58bd
puskini33 <elenahirjoaba@yahoo.com>
2020-02-27 16:02:07 +0100

    add nodes and connect_nodes possibility

--- view/main.py
+++ view/main.py
@@ -1,26 +1,52 @@
 from tkinter import *
 
 
-def draw_line(event):
+def reset_circle():
+    global clicked_circles
+    for circle in clicked_circles:
+        my_canvas.itemconfigure(circle, fill='black')
+    clicked_circles = []
 
+
+def select_circles(event):
     x, y = event.x, event.y
-    if my_canvas.old_coordinates:
-        x1, y1 = my_canvas.old_coordinates
-        my_canvas.create_line(x, y, x1, y1, fill='black', width=3)
-        my_canvas.old_coordinates = None
-        return
-    my_canvas.old_coordinates = x, y
+    selection = my_canvas.find_overlapping(x, y, x, y)
+    if selection is not None and len(clicked_circles) < 2:
+        clicked_circles.append(selection)
+    if len(clicked_circles) == 2:
+        if all(clicked_circles):
+            join_circles()
 
 
-def draw_circle(event):
+def join_circles():
+    coordinates = []
+    for circle in clicked_circles:
+        x, y = find_center(circle)
+        coordinates.append(x)
+        coordinates.append(y)
+    my_canvas.create_line(coordinates, tag='all')
+    reset_circle()
+
+
+def find_center(item):
+    x0, y0, x1, y1 = my_canvas.bbox(item)
+    return ((x0 + 0.01) + (x1 + 0.01)) / 2, ((y0 + 0.01) + (y1 + 0.01)) / 2
+
+
+def draw_oval(event):
     x, y = event.x, event.y
-    my_canvas.create_oval(x, y, x + 50, y + 50, fill='black', width=3)
+    my_canvas.create_oval(x - 25, y - 25, x + 25, y + 25, fill='black', width=3, tag='all')
 
+def delete_all(event):
+    my_canvas.delete('all')
 
 my_window = Tk()
 my_canvas = Canvas(my_window, width=600, height=600, bg='white')
 my_canvas.grid(row=0, column=0)
 my_canvas.old_coordinates = None
-my_canvas.bind('<Button-1>', draw_line)
-my_canvas.bind('<Button-3>', draw_circle)
+my_canvas.bind('<Button-3>', draw_oval)
+my_canvas.bind('<Button-1>', select_circles)
+my_canvas.bind('<Button-2>', delete_all)
+
+clicked_circles = []
 my_window.mainloop()
