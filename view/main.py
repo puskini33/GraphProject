ce2def041151060fb1612c592e2e171b563517b6
puskini33 <elenahirjoaba@yahoo.com>
2020-02-27 23:32:57 +0100

    add submenu, change colors circles

--- view/main.py
+++ view/main.py
@@ -1,65 +1,98 @@
+from tkinter import filedialog
 from tkinter import *
 
 
-def reset_circle():
-    global clicked_circles
-    clicked_circles = []
+class Graph(Frame):
+
+    def __init__(self, window):
+        self.window = window
+        self.canvas = Canvas(self.window, width=700, height=700, bg='white')
+        self.clicked_circles = []
+        super().__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        self.canvas.grid(row=0, column=0)
+        self.canvas.old_coordinates = None
+        self.canvas.bind('<Button-3>', self.draw_oval)
+        self.canvas.bind('<Button-1>', self.select_circles)
+        self.canvas.bind('<Button-2>', self.delete_all)
+
+        self.master.title('GraphIt')
+        menu_bar = Menu(self.master)
+        self.master.config(menu=menu_bar)
+
+        drop_down_menu = Menu(menu_bar)
+        menu_bar.add_cascade(label="File", menu=drop_down_menu)
+        drop_down_menu.add_command(label="Save", command=self.save_graphit)
+        drop_down_menu.add_command(label="Open..", command=self.open_graphit)
+        drop_down_menu.add_command(label="Exit", command=self.exit_graphit)
+
+    def exit_graphit(self):
+        self.quit()
+
+    def save_graphit(self):
+        """self.window.filename = filedialog.asksaveasfilename(initialdir="/", title="Select file",
+                                                            filetypes=(("jpeg files", "*.jpg"), ("all files", "*.*")),
+                                                            confirmoverwrite=False)"""
 
 
-def select_circles(event):
-    x, y = event.x, event.y
-    selection = my_canvas.find_overlapping(x, y, x, y)
-    if len(selection) == 0:
-        return
-    elif len(selection) != 0 and len(clicked_circles) < 2:
-        clicked_circles.append(selection)
-    if len(clicked_circles) == 2:
-        if all(clicked_circles):
-            join_circles()
-    elif len(clicked_circles) == 2 and clicked_circles[0] == clicked_circles[1]:
-        return
 
+    def open_graphit(self):
+        self.window.filename = filedialog.askopenfilename(initialdir="/", title="Select file",
+                                                          filetypes=(("jpeg files", "*.jpg"), ("all files", "*.*")))
 
-def join_circles():
-    global clicked_circles
-    coordinates = []
-    for circle in clicked_circles:
-        try:
-            x, y = find_center(circle)
-            coordinates.append(x)
-            coordinates.append(y)
-        except TypeError:
-            reset_circle()
+    def reset_circle(self):
+        self.clicked_circles = []
+
+    def select_circles(self, event):
+        x, y = event.x, event.y
+        selection = self.canvas.find_overlapping(x, y, x, y)
+        if len(selection) == 0:
             return
-    my_canvas.create_line(coordinates, tag='all')
-    reset_circle()
+        elif len(selection) != 0 and len(self.clicked_circles) < 2:
+            self.clicked_circles.append(selection)
+        if len(self.clicked_circles) == 2:
+            if all(self.clicked_circles):
+                self.join_circles()
+        elif len(self.clicked_circles) == 2 and self.clicked_circles[0] == self.clicked_circles[1]:
+            return
+
+    def join_circles(self):
+        coordinates = []
+        for circle in self.clicked_circles:
+            try:
+                x, y = self.find_center(circle)
+                coordinates.append(x)
+                coordinates.append(y)
+            except TypeError:
+                self.reset_circle()
+                return
+        self.canvas.create_line(coordinates, tag='all', arrow='last', width=3)
+        self.reset_circle()
+
+    def find_center(self, item):
+        if self.canvas.bbox(item) is None:
+            return
+        x0, y0, x1, y1 = self.canvas.bbox(item)
+        coord_x = ((x0 + 0.01) + (x1 + 0.01)) / 2
+        coord_y = ((y0 + 0.01) + (y1 + 0.01)) / 2
+        return coord_x, coord_y
+
+    def draw_oval(self, event):
+        x, y = event.x, event.y
+        self.canvas.create_oval(x - 25, y - 25, x + 25, y + 25, fill='orange', width=3, tag='all')
+
+    def delete_all(self, event):
+        self.canvas.delete('all')
 
 
-def find_center(item):
-    if my_canvas.bbox(item) is None:
-        return
-    x0, y0, x1, y1 = my_canvas.bbox(item)
-    coord_x = ((x0 + 0.01) + (x1 + 0.01)) / 2
-    coord_y = ((y0 + 0.01) + (y1 + 0.01)) / 2
-    return coord_x, coord_y
+def main():
+    window = Tk()
+    window.geometry("700x700")
+    graph= Graph(window)
+    window.mainloop()
 
 
-def draw_oval(event):
-    x, y = event.x, event.y
-    my_canvas.create_oval(x - 25, y - 25, x + 25, y + 25, fill='black', width=3, tag='all')
-
-
-def delete_all(event):
-    my_canvas.delete('all')
-
-
-my_window = Tk()
-my_canvas = Canvas(my_window, width=600, height=600, bg='white')
-my_canvas.grid(row=0, column=0)
-my_canvas.old_coordinates = None
-my_canvas.bind('<Button-3>', draw_oval)
-my_canvas.bind('<Button-1>', select_circles)
-my_canvas.bind('<Button-2>', delete_all)
-
-clicked_circles = []
-my_window.mainloop()
+if __name__ == '__main__':
+    main()
--- view/trial.py
+++ view/trial.py
@@ -1,82 +1,59 @@
-import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
+
+filename = ''
+
+form = Tk()
+form.geometry('1000x600')
+form.title('Text Editor')
+frame = ttk.Frame(form, padding="3 3 12 12")
+frame.grid(column=0, row=0, sticky=(N, W, E, S))
+frame.columnconfigure(0, weight=1)
+frame.rowconfigure(0, weight=1)
+textentry = Text(frame, width=95, height=35)
+textentry.grid(column=0, row=0, columnspan=4, sticky=(N, W, E, S))
+textentry.focus()
 
 
-class CommandButtons(tk.Frame):
-    def __init__(self, master):
-        self.master = master
-        super().__init__(self.master)
-        self.make_circle_btn = tk.Button(root, text='make_circle', command=make_circle)
-        self.make_circle_btn.pack()
-        self.join_circles_btn = tk.Button(root, text='join_circles', command=select_circles)
-        self.join_circles_btn.pack()
-        self.pack()
+def file_new():
+    global filename
+    textentry.delete('1.0', 'end')
+    filename = ''
 
 
-class CircleApp(tk.Frame):
-    def __init__(self, master):
-        self.master = master
-        super().__init__(self.master)
-        self.canvas = tk.Canvas(root, width=600, height=600, bg='cyan')
-        self.canvas.pack(expand=True, fill=tk.BOTH)
-        self.pack()
+def file_open():
+    global filename
+    filename = filedialog.askopenfile(mode='r+')
+    if filename is not None:
+        t = filename.read()
+        textentry.delete('0.0', 'end')
+        textentry.insert('0.0', t)
+        textentry.focus()
 
 
-def make_circle():
-    _purge_selection()
-    print('cmd make_circle selected')
-    c_app.canvas.bind('<Button-1>', _make_circle)
+def file_save():
+    global filename
+    if filename == '':
+        filename = filedialog.asksaveasfile(mode='w')
+    if filename is not None:
+        data = textentry.get('1.0', 'end')
+        filename.write(data)
 
 
-def _make_circle(event):
-    c_app.canvas.create_oval(event.x - 25, event.y - 25, event.x + 25, event.y + 25, fill="blue")
+def file_save_as():
+    global filename
+    filename = filedialog.asksaveasfile(mode='w')
+    file_save()
 
 
-def select_circles():
-    _purge_selection()
-    print('cmd join_circles selected')
-    c_app.canvas.bind('<Button-1>', _select_circles)
+btnNew = ttk.Button(frame, text='New', command=file_new)
+btnNew.grid(column=0, row=1, sticky='S')
+btnOpen = ttk.Button(frame, text='Open', command=file_open)
+btnOpen.grid(column=1, row=1, sticky='S')
+btnSave = ttk.Button(frame, text='Save', command=file_save)
+btnSave.grid(column=2, row=1, sticky='S')
+btnSaveAs = ttk.Button(frame, text='Save As', command=file_save_as)
+btnSaveAs.grid(column=3, row=1, sticky='S')
 
-
-def _select_circles(event):
-    print(f'select circle {event}')
-    x, y = event.x, event.y
-    selection = c_app.canvas.find_overlapping(x, y, x, y)
-    if selection is not None and len(selected_circles) < 2:
-        selected_circles.append(selection)
-        c_app.canvas.itemconfigure(selection, fill='red')
-    if len(selected_circles) == 2:
-        if all(selected_circles):
-            _join_circles()
-        _purge_selection()
-
-
-def _join_circles():
-    coordinates = []
-    for item in selected_circles:
-        x, y = find_center(item)
-        print(x, y)
-        coordinates.append(x)
-        coordinates.append(y)
-    c_app.canvas.create_line(coordinates)
-
-
-def _purge_selection():
-    global selected_circles
-    for item in selected_circles:
-        c_app.canvas.itemconfigure(item, fill='blue')
-    selected_circles = []
-
-
-def find_center(item):
-    x0, y0, x1, y1 = c_app.canvas.bbox(item)
-    return (x0 + x1) / 2, (y0 + y1) / 2
-
-
-if __name__ == '__main__':
-    selected_circles = []
-
-    root = tk.Tk()
-    command_frame = CommandButtons(root)
-    c_app = CircleApp(root)
-
-    root.mainloop()
+form.mainloop()
