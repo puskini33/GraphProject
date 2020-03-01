d69ac21c23920fbb27e0572c10ed52683236c5d7
puskini33 <elenahirjoaba@yahoo.com>
2020-03-01 23:43:55 +0100

    update models

--- models/graph_model.py
+++ models/graph_model.py
@@ -1,8 +1,13 @@
+from models.node_model import NodeModel
+from typing import List
+
+
 class GraphModel(object):
 
     def __init__(self, graph_id: int = -1, graph_name: str = ''):
         self.graph_id = graph_id
         self.graph_name = graph_name
+        self.list_of_nodes: List[NodeModel] = []
 
     def __repr__(self):
         return f'Graph Id: {self.graph_id} , Graph Name: {self.graph_name}'
--- models/node_model.py
+++ models/node_model.py
@@ -1,9 +1,20 @@
+from models.edge_model import EdgeModel
+
+
 class NodeModel(object):
 
     def __init__(self, node_id: int = -1, node_name: str = '', graph_id: int = -1):
         self.node_id = node_id
         self.node_name = node_name
         self.graph_id = graph_id
+        self.x_coord = 0
+        self.y_coord = 0
+        self.list_of_edges: List[EdgeModel] = []
 
     def __repr__(self):
-        return f'[Node Id: {self.node_id}, Node Name: {self.node_name}, Graph Id: {self.graph_id}]'
+        return f'[Node Id: {self.node_id}, Node Name: {self.node_name}, X Coord: {self.x_coord} ' \
+               f'Y Coord: {self.y_coord}, Graph Id: {self.graph_id}]'
+
+    def set_coord(self, x: int, y: int):
+        self.x_coord = x
+        self.y_coord = y
