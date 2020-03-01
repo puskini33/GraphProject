a77edbe7ba9a18864fb79c54de41c8fb2f7aa749
puskini33 <elenahirjoaba@yahoo.com>
2020-03-01 23:44:28 +0100

    add tkinter interface

--- view/graph_view.py
+++ view/graph_view.py
@@ -1,85 +1,77 @@
-from tkinter import filedialog
+from models.graph_model import GraphModel
+from models.node_model import NodeModel
+from models.edge_model import EdgeModel
+from application_service.graph_application_service import GraphApplication
 from tkinter import *
 
 
-class Graph(Entry):
+class GraphView(Frame):
 
-    def __init__(self, window):
+    def __init__(self, window, graph_model: GraphModel, graph_app_service: GraphApplication):
         self.window = window
         self.canvas = Canvas(self.window, width=700, height=700, bg='white')
-        self.clicked_circles = []
-        self.changes = [""]
-        self.steps = int()
+        self.graph_model = graph_model
+        self.graph_app_service = graph_app_service
+        self.selected_circles = []
+        self.node_radius = 25
         super().__init__()
         self.init_ui()
 
     def init_ui(self):
         self.canvas.grid(row=0, column=0)
-        self.canvas.old_coordinates = None
-        self.canvas.bind('<Button-3>', self.draw_oval)
-        self.canvas.bind('<Button-1>', self.select_circles)
-        self.canvas.bind('<Button-2>', self.delete_all)
-        self.bind("<Control-z>", self.undo)
-        self.bind("<Key>", self.add_changes)
-
+        self.canvas.bind('<Button-3>', self.right_click_event_handler)
+        self.canvas.bind('<Button-1>', self.left_click_event_handler)
         self.master.title('GraphIt')
+        self.add_menu_bar()
+        self.draw_graph(self.graph_model)
+
+    def add_menu_bar(self):
         menu_bar = Menu(self.master)
         self.master.config(menu=menu_bar)
 
         drop_down_menu = Menu(menu_bar)
         menu_bar.add_cascade(label="File", menu=drop_down_menu)
-        # drop_down_menu.add_command(label="Undo", command=self.undo)
-        drop_down_menu.add_command(label="Open..", command=self.open_graphit)
-        drop_down_menu.add_command(label="Exit", command=self.exit_graphit)
+        drop_down_menu.add_command(label="Save", command=self.save_graph)
+        drop_down_menu.add_command(label="Load Graph", command=self.get_graph)
 
-    def undo(self, event=None):
-        if self.steps != 0:
-            self.steps -= 1
-            self.delete(0, END)
-            self.insert(END, self.changes[self.steps])
-
-    def add_changes(self, event=None):
-        if self.get() != self.changes[-1]:
-            self.changes.append(self.get())
-            self.steps += 1
-
-    def exit_graphit(self):
-        self.quit()
-
-    def open_graphit(self):
-        self.window.filename = filedialog.askopenfilename(initialdir="/", title="Select file",
-                                                          filetypes=(("jpeg files", "*.jpg"), ("all files", "*.*")))
-
-    def reset_circle(self):
-        self.clicked_circles = []
-
-    def select_circles(self, event):
+    def right_click_event_handler(self, event):
         x, y = event.x, event.y
+        node_model = NodeModel(node_name='TrialNode')
+        node_model.set_coord(x, y)
+        self.graph_model.list_of_nodes.append(node_model)
+        self.draw_node(node_model)
+
+    def left_click_event_handler(self, event):
+        x, y = event.x, event.y
+        edge_model = EdgeModel(edge_name='TrialEdge')
+        self.select_node(x, y)
+
+    def select_node(self, x, y):
         selection = self.canvas.find_overlapping(x, y, x, y)
         if len(selection) == 0:
             return
-        elif len(selection) != 0 and len(self.clicked_circles) < 2:
-            self.clicked_circles.append(selection)
-        if len(self.clicked_circles) == 2:
-            if all(self.clicked_circles):
+        elif len(selection) != 0 and len(self.selected_circles) < 2:
+            self.selected_circles.append(selection)
+        if len(self.selected_circles) == 2:
+            if all(self.selected_circles):
                 self.join_circles()
-        elif len(self.clicked_circles) == 2 and self.clicked_circles[0] == self.clicked_circles[1]:
+        elif len(self.selected_circles) == 2 and self.selected_circles[0] == self.selected_circles[1]:
             return
 
     def join_circles(self):
         coordinates = []
-        for circle in self.clicked_circles:
+        for circle in self.selected_circles:
             try:
-                x, y = self.find_center(circle)
+                x, y = self.calculate_center_node(circle)
                 coordinates.append(x)
                 coordinates.append(y)
             except TypeError:
-                self.reset_circle()
+                self.selected_circles = []
                 return
         self.canvas.create_line(coordinates, tag='all', arrow='last', width=3)
-        self.reset_circle()
+        self.selected_circles = []
 
-    def find_center(self, item):
+    def calculate_center_node(self, item):
         if self.canvas.bbox(item) is None:
             return
         x0, y0, x1, y1 = self.canvas.bbox(item)
@@ -87,20 +79,16 @@ class Graph(Entry):
         coord_y = ((y0 + 0.01) + (y1 + 0.01)) / 2
         return coord_x, coord_y
 
-    def draw_oval(self, event):
-        x, y = event.x, event.y
-        self.canvas.create_oval(x - 25, y - 25, x + 25, y + 25, fill='orange', width=3, tag='all')
+    def draw_graph(self, graph_model: GraphModel):
+        for node_model in graph_model.list_of_nodes:
+            self.draw_node(node_model)
 
-    def delete_all(self, event):
-        self.canvas.delete('all')
+    def draw_node(self, node_model):
+        self.canvas.create_oval(node_model.x_coord - self.node_radius, node_model.y_coord - self.node_radius,
+                                node_model.x_coord + self.node_radius, node_model.y_coord + self.node_radius, fill='orange', width=3, tag='all')
 
+    def save_graph(self):
+        self.graph_app_service.save_graph_model(self.graph_model)
 
-def main():
-    window = Tk()
-    window.geometry("700x700")
-    graph= Graph(window)
-    window.mainloop()
-
-
-if __name__ == '__main__':
-    main()
+    def get_graph(self):
+        pass
--- view/trial.py
+++ view/trial.py
@@ -1,59 +1,97 @@
-from tkinter import *
-from tkinter import ttk
 from tkinter import filedialog
-
-filename = ''
-
-form = Tk()
-form.geometry('1000x600')
-form.title('Text Editor')
-frame = ttk.Frame(form, padding="3 3 12 12")
-frame.grid(column=0, row=0, sticky=(N, W, E, S))
-frame.columnconfigure(0, weight=1)
-frame.rowconfigure(0, weight=1)
-textentry = Text(frame, width=95, height=35)
-textentry.grid(column=0, row=0, columnspan=4, sticky=(N, W, E, S))
-textentry.focus()
+from tkinter import *
 
 
-def file_new():
-    global filename
-    textentry.delete('1.0', 'end')
-    filename = ''
+class GraphView(Frame):
+
+    def __init__(self, window):
+        self.window = window
+        self.canvas = Canvas(self.window, width=700, height=700, bg='white')
+        self.clicked_circles = []
+        self.changes = [""]
+        self.steps = int()
+        super().__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        self.canvas.grid(row=0, column=0)
+        self.canvas.old_coordinates = None
+        self.canvas.bind('<Button-3>', self.draw_oval)
+        self.canvas.bind('<Button-1>', self.select_circles)
+        self.canvas.bind('<Button-2>', self.delete_all)
+        # self.bind("<Control-z>", self.undo)
+        # self.bind("<Key>", self.add_changes)
+
+        self.master.title('GraphIt')
+        menu_bar = Menu(self.master)
+        self.master.config(menu=menu_bar)
+
+        drop_down_menu = Menu(menu_bar)
+        menu_bar.add_cascade(label="File", menu=drop_down_menu)
+        # drop_down_menu.add_command(label="Undo", command=self.undo)
+        drop_down_menu.add_command(label="Open..", command=self.open_graphit)
+        drop_down_menu.add_command(label="Exit", command=self.exit_graphit)
+
+    """def undo(self, event=None):
+        if self.steps != 0:
+            self.steps -= 1
+            self.delete(0, END)
+            self.insert(END, self.changes[self.steps])
+
+    def add_changes(self, event=None):
+        if self.get() != self.changes[-1]:
+            self.changes.append(self.get())
+            self.steps += 1"""
+
+    def exit_graphit(self):
+        self.quit()
+
+    def open_graphit(self):
+        self.window.filename = filedialog.askopenfilename(initialdir="/", title="Select file",
+                                                          filetypes=(("jpeg files", "*.jpg"), ("all files", "*.*")))
+
+    def reset_circle(self):
+        self.clicked_circles = []
+
+    def select_circles(self, event):
+        x, y = event.x, event.y
+        selection = self.canvas.find_overlapping(x, y, x, y)
+        if len(selection) == 0:
+            return
+        elif len(selection) != 0 and len(self.clicked_circles) < 2:
+            self.clicked_circles.append(selection)
+        if len(self.clicked_circles) == 2:
+            if all(self.clicked_circles):
+                self.join_circles()
+        elif len(self.clicked_circles) == 2 and self.clicked_circles[0] == self.clicked_circles[1]:
+            return
+
+    def join_circles(self):
+        coordinates = []
+        for circle in self.clicked_circles:
+            try:
+                x, y = self.find_center(circle)
+                coordinates.append(x)
+                coordinates.append(y)
+            except TypeError:
+                self.reset_circle()
+                return
+        self.canvas.create_line(coordinates, tag='all', arrow='last', width=3)
+        self.reset_circle()
+
+    def find_center(self, item):
+        if self.canvas.bbox(item) is None:
+            return
+        x0, y0, x1, y1 = self.canvas.bbox(item)
+        coord_x = ((x0 + 0.01) + (x1 + 0.01)) / 2
+        coord_y = ((y0 + 0.01) + (y1 + 0.01)) / 2
+        return coord_x, coord_y
+
+    def draw_oval(self, event):
+        x, y = event.x, event.y
+        self.canvas.create_oval(x - 25, y - 25, x + 25, y + 25, fill='orange', width=3, tag='all')
+
+    def delete_all(self, event):
+        self.canvas.delete('all')
 
 
-def file_open():
-    global filename
-    filename = filedialog.askopenfile(mode='r+')
-    if filename is not None:
-        t = filename.read()
-        textentry.delete('0.0', 'end')
-        textentry.insert('0.0', t)
-        textentry.focus()
-
-
-def file_save():
-    global filename
-    if filename == '':
-        filename = filedialog.asksaveasfile(mode='w')
-    if filename is not None:
-        data = textentry.get('1.0', 'end')
-        filename.write(data)
-
-
-def file_save_as():
-    global filename
-    filename = filedialog.asksaveasfile(mode='w')
-    file_save()
-
-
-btnNew = ttk.Button(frame, text='New', command=file_new)
-btnNew.grid(column=0, row=1, sticky='S')
-btnOpen = ttk.Button(frame, text='Open', command=file_open)
-btnOpen.grid(column=1, row=1, sticky='S')
-btnSave = ttk.Button(frame, text='Save', command=file_save)
-btnSave.grid(column=2, row=1, sticky='S')
-btnSaveAs = ttk.Button(frame, text='Save As', command=file_save_as)
-btnSaveAs.grid(column=3, row=1, sticky='S')
-
-form.mainloop()
