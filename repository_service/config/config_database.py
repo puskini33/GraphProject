b227aaecc9d354934d653971ab9501a269142834
puskini33 <elenahirjoaba@yahoo.com>
2020-06-13 20:52:03 +0200

    add relative path to graph database

--- repository_service/config/config_database.py
+++ repository_service/config/config_database.py
@@ -1,9 +1,13 @@
 import os
+from pathlib import Path
 
 
 class ConfigDatabase(object):
 
+    base_path = Path(__file__).parent
+    database_path = (base_path / "../../SQL").resolve()
+
     @staticmethod
-    def get_path(path: str = 'E:\\PYTHON\\code\\GraphProject\\SQL') -> str:
+    def get_path(path: str = database_path) -> str:
         default_path = os.path.join(path, 'graph.db')
         return default_path
